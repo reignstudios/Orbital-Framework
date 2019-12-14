a95a1c286dcd8da579fc99998757d4f87216b30f
Andrew <zezba9000@gmail.com>
2019-12-13 19:48:58 -0800

    CS2X runtime work

--- Platforms/Shared/Orbital.Video/RenderPass.cs
+++ Platforms/Shared/Orbital.Video/RenderPass.cs
@@ -1,7 +1,15 @@
-﻿using System;
+﻿using Orbital.Numerics;
+using System;
 
 namespace Orbital.Video
 {
+	public struct RenderPassDesc
+	{
+		public bool clearColor, clearDepthStencil;
+		public Vec4 clearColorValue;
+		public float depthValue, stencilValue;
+	}
+
 	public abstract class RenderPassBase : IDisposable
 	{
 		public abstract void Dispose();
