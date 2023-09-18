201b03e96efa0873569c4f438db76090a123c15b
Andrew Witte <zezba9000@gmail.com>
2023-09-17 21:06:52 -0700

    Prep. geting things compiling again

--- Platforms/Shared/Orbital.Networking.Sockets/RUDPSocketServer.cs
+++ Platforms/Shared/Orbital.Networking.Sockets/RUDPSocketServer.cs
@@ -1,7 +1,11 @@
-﻿namespace Orbital.Networking.Sockets
+﻿using System.Net;
+
+namespace Orbital.Networking.Sockets
 {
 	public class RUDPSocketServer : RUDPSocket
 	{
-
+		public RUDPSocketServer(IPAddress remoteAddress, IPAddress localAddress, int port, int receiveBufferSize) : base(remoteAddress, localAddress, port, receiveBufferSize)
+		{
+		}
 	}
 }
