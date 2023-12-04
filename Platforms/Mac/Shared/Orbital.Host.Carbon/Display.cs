c2ee72d217e0e50298167f6faed758616bb705ea
Andrew <zezba9000@gmail.com>
2023-12-04 01:54:35 -0800

    mac Display API prep

---
+++ Platforms/Mac/Shared/Orbital.Host.Carbon/Display.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Orbital.Host.Carbon
+{
+	public static class Displays
+	{
+		public static Display GetPrimaryDisplay()
+		{
+			return new Display();
+		}
+
+		public static Display[] GetDisplays()
+		{
+			return null;
+		}
+	}
+}
---
+++ Platforms/Mac/Shared/Orbital.Host.Cocoa/Display.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Orbital.Host.Cocoa
+{
+	public static class Displays
+	{
+		public static Display GetPrimaryDisplay()
+		{
+			return new Display();
+		}
+
+		public static Display[] GetDisplays()
+		{
+			return null;
+		}
+	}
+}
