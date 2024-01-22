7af5bdb00f0fdcc69d71dae9ec1ec6dfd7310513
Andrew <zezba9000@gmail.com>
2024-01-22 00:34:02 -0800

    wayland prep

---
+++ Platforms/Lin/NetCore/Orbital.Demo.Wayland/Program.cs
@@ -0,0 +1,21 @@
+﻿using Orbital.Host;
+using Orbital.Host.Wayland;
+
+namespace Orbital.Demo.Wayland
+{
+    static class Program
+    {
+        private static void Main(string[] args)
+        {
+            // init app and window
+            Application.Init();
+            var window = new Window(320, 240, WindowType.Tool, WindowStartupPosition.CenterScreen);
+            window.SetTitle("Demo: Wayland");
+            window.Show();
+            
+            // run app till quit
+            Application.Run(window);
+            Application.Shutdown();
+        }
+    }
+}
