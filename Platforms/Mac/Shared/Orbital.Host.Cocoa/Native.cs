a244ae6c1260680b7c556e17fe8d13c95224a224
Andrew Witte <zezba9000@gmail.com>
2024-04-14 23:33:56 -0700

    Refactored Cocoa native project names. Corrected Cocoa Demo native lib linking

--- Platforms/Mac/Shared/Orbital.Host.Cocoa/Native.cs
+++ Platforms/Mac/Shared/Orbital.Host.Cocoa/Native.cs
@@ -2,6 +2,6 @@
 {
 	public static class Native
 	{
-		public const string lib = "../../../../../Native/Orbital.Host.Cocoa.Native/Host/DerivedData/Host/Build/Products/Debug/libHost.dylib";
+		public const string lib = "libOrbital.Host.Cocoa.Native.dylib";
 	}
 }
